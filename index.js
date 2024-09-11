6b437b980246f324badc162ee09e38049e7394ca
Samiksha Tikhile <150263722+Samikshatikhile@users.noreply.github.com>
2024-09-11 23:05:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var tl = gsap.timeline();
+
+tl.to("#page1", {
+    y: "100vh",
+    scale: 0.6,
+    duration: 0
+})
+.to("#page1", {
+    y: "30vh",
+    duration: 1,
+    delay: 1
+})
+.to("#page1", {
+    y: "0vh",
+    rotate: 360,
+    scale: 1,
+    duration: 0.7
+});
